327afe1b9c3f5fd0af993701698ff52a17ba5f9c
USAMI Kenta <tadsan@zonu.me>
2015-08-22 16:03:47 +0900

    Require request

---
+++ Cask
@@ -0,0 +1,15 @@
+(source gnu)
+(source melpa)
+
+(package "twindrill-mode" "4.0.0" "Twitter client for Emacs")
+(package-file "twindrill-mode.el")
+
+(depends-on "dash")
+(depends-on "request")
+(depends-on "s")
+
+(development
+ (depends-on "ert")
+ (depends-on "ert-runner")
+ (depends-on "noflet")
+ (depends-on "undercover"))
--- twindrill-mode.el
+++ twindrill-mode.el
@@ -15,6 +15,8 @@
 ;; Identity: $Id$
 ;; Keywords: twitter web
 ;; URL: https://github.com/zonuexe/twindrill-mode
+;; Package-Requires: ((emacs "24") (request "0.2.0") (dash "2.9.0") (s "1.9.0"))
+
 
 ;; This program is free software: you can redistribute it and/or modify
 ;; it under the terms of the GNU General Public License as published by
